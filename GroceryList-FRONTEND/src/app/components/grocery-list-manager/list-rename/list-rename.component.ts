e61c29105134ce3ea4f5794de8f60ec4ed133b6a
Oleiny Carrasco <oocarrazco@gmail.com>
2025-07-16 03:21:43 -0400

    Implemented frontend Grocery List application with core components and services

---
+++ GroceryList-FRONTEND/src/app/components/grocery-list-manager/list-rename/list-rename.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-rename',
+  templateUrl: './list-rename.component.html',
+  styleUrls: ['./list-rename.component.css'],
+  standalone: true
+})
+export class ListRenameComponent {}
