eb2be1076a354ca5f742ee8921286c52b171d7b0
Oleiny Carrasco <oocarrazco@gmail.com>
2025-07-18 14:45:46 -0400

    Unify dashboard and grocery list manager HTML

--- GroceryList-FRONTEND/src/app/components/dashboard/dashboard.component.ts
+++ GroceryList-FRONTEND/src/app/components/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, DestroyRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroceryListService } from '../../services/grocery-list.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { GroceryList } from '../../models/grocery-list.model';
 import { GroceryListManagerComponent } from '../grocery-list-manager/grocery-list-manager.component';
 
 @Component({
@@ -13,20 +12,8 @@ import { GroceryListManagerComponent } from '../grocery-list-manager/grocery-lis
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  groceryLists: GroceryList[] = [];
   constructor(private groceryListService: GroceryListService) {}
   private readonly destroyRef = inject(DestroyRef);
 
-  ngOnInit(): void {
-    this.loadLists();
-  }
-
-  loadLists(): void {
-    this.groceryListService.getGroceryLists().pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
-      next: (lists) => {
-        this.groceryLists = lists.filter(l => l != null);
-      },
-      error: (err) => console.error('Error loading grocery lists:', err)
-    });
-  }
+  ngOnInit(): void {}
 } 
